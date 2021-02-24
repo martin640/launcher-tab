275306b11b46b919437ae676fca85dba19be9203
natanparrondo <natanparrondo@gmail.com>
2021-02-24 14:53:01 -0300

    removed fake attribution gearth

--- src/res/base.js
+++ src/res/base.js
@@ -248,12 +248,10 @@ class TabContext {
             const idsList = (await (await fetch(`/res/gearthids.json`)).json()).ids
             if (!Array.isArray(idsList)) return error()
             const randomId = idsList[Math.floor(Math.random() * idsList.length)]
-            const gearththirds = ["TerraMetrics","CNES","Airbus","Maxar Technologies","Landsat","Copernicus", ]
-            const gearthatt = gearththirds[Math.floor(Math.random() * gearththirds.length)]
 
             return {
                 src: `https://www.gstatic.com/prettyearth/assets/full/${randomId}.jpg`,
-                attribution: `<a href="https://earth.google.com/web/">Google Earth (Map data ©2020 Google) ${gearthatt}</a>`
+                attribution: `<a href="https://earth.google.com/web/">Google Earth (Map data ©2020 Google)</a>`
             }
         }
         const fetchFromUrl = async () => {
